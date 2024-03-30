9b8dac1b9ce5c2b6f45e14825aefdb502a853158
yuuki.kato <0me3210g039530z@gmail.com>
2024-03-30 16:34:39 +0900

    first svelte app

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  let books = ["ドグラ・マグラ", "黒死館殺人事件", "虚無への供物"];
+  let newBook = "";
+
+  function addBook(evt) {
+    if (evt.key === "Enter") {
+      books = [...books, newBook];
+      newBook = "";
+    }
+  }
+</script>
+
+<label>
+  <h4>Add Book</h4>
+  <input type="text" bind:value={newBook} on:keydown={addBook} />
+</label>
+<h4>My Books</h4>
+<ul>
+  {#each books as book}
+    <li transition:fade>{book}</li>
+  {/each}
+</ul>
+
+<style>
+  input {
+    padding: 5px 10px;
+  }
+  li {
+    list-style: none;
+  }
+  ul {
+    padding: 5px 0;
+  }
+</style>
